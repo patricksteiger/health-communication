28834aa47f9e59a7e7417cc060ee032946bf116b
Patrick Steiger <patricksteiger7@gmail.com>
2021-11-13 19:50:22 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Main');
+  // port
   const port = 3000;
   await app.listen(port, () => logger.log(`Running on port ${port}.`));
 }
